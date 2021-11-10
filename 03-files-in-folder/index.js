43f9ba90ba268463bca459a0f9053fae412a068b
Developer4you <Avervalpet@gmail.com>
2021-11-10 14:03:45 +0200

    Update index.js

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,23 @@
-const fs = require('fs');
-const path = require('path');
-const filePath = path.resolve(__dirname, 'secret-folder');
+const fs = require("fs");
+const path = require("path");
+const filePath = path.resolve(__dirname, "secret-folder");
 
-async function getFiles (){
+async function getFiles() {
   try {
     const files = await fs.promises.readdir(filePath);
-    for (const file of files){
+    for (const file of files) {
       const stat = await fs.promises.stat(`${filePath}/${file}`);
       if (!stat.isDirectory()) {
-        let filesInformation = `${file.split('.')[0]} - ${file.split('.')[1]} - ${stat.size/1000}kb`;
-        console.log(filesInformation);}
+        let filesInformation = `${path.basename(
+          file,
+          path.extname(file)
+        )} - ${path.extname(file).slice(1, file.length - 1)} - ${
+          stat.size / 1000
+        }kb`;
+        console.log(filesInformation);
+      }
     }
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err);
   }
 }
